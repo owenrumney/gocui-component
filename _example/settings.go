dcda3e65c77d13b818f365815ab398003ea9fb74
Owen Rumney <owen@owenrumney.co.uk>
2022-09-14 14:28:12 +0100

    Update settings.go

--- _example/settings.go
+++ _example/settings.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/awesome-gocui/gocui"
-	component "github.com/owenrumney/gocui-component"
+	component "github.com/owenrumney/gocui-form"
 )
 
 type SettingsWidget struct {
